7906135da07a03c1e97bcb0609a51ffbc7929e08
Dakkshesh <dakkshesh5@gmail.com>
2023-08-09 17:27:16 +0000

    Revert "gralloc: Upgrade mapper and allocators to v3.0"
    This reverts commit e65f023168f676374f1ad081cec3d1723204f2bf.

--- gralloc/Android.mk
+++ gralloc/Android.mk
@@ -12,8 +12,7 @@ LOCAL_C_INCLUDES              := $(common_includes)
 LOCAL_HEADER_LIBRARIES        := display_headers
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libqdMetaData libsync libgrallocutils \
                                   libgralloccore android.hardware.graphics.mapper@2.0 \
-                                 android.hardware.graphics.mapper@2.1 \
-                                 android.hardware.graphics.mapper@3.0
+                                 android.hardware.graphics.mapper@2.1
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdgralloc\" -Wall -std=c++14 -Werror
 LOCAL_CFLAGS                  += -isystem  $(kernel_includes)
 LOCAL_CLANG                   := true
@@ -30,8 +29,7 @@ LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
 LOCAL_HEADER_LIBRARIES        := display_headers
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libqdMetaData libdl  \
                                   android.hardware.graphics.mapper@2.0 \
-                                  android.hardware.graphics.mapper@2.1 \
-                                  android.hardware.graphics.mapper@3.0
+                                  android.hardware.graphics.mapper@2.1
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdgralloc\" -Wno-sign-conversion
 
 ifeq ($(TARGET_USES_ALIGNED_YCRCB_HEIGHT),true)
@@ -60,8 +58,7 @@ LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
 LOCAL_HEADER_LIBRARIES        := display_headers
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libqdMetaData libdl libgrallocutils \
                                   android.hardware.graphics.mapper@2.0 \
-                                  android.hardware.graphics.mapper@2.1 \
-                                  android.hardware.graphics.mapper@3.0
+                                  android.hardware.graphics.mapper@2.1
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdgralloc\" -Wno-sign-conversion
 
 ifeq ($(TARGET_USES_YCRCB_CAMERA_PREVIEW),true)
@@ -76,7 +73,7 @@ include $(BUILD_SHARED_LIBRARY)
 
 #mapper
 include $(CLEAR_VARS)
-LOCAL_MODULE                  := android.hardware.graphics.mapper@3.0-impl-qti-display
+LOCAL_MODULE                  := android.hardware.graphics.mapper@2.0-impl-qti-display
 LOCAL_VENDOR_MODULE           := true
 LOCAL_MODULE_RELATIVE_PATH    := hw
 LOCAL_MODULE_TAGS             := optional
@@ -89,12 +86,11 @@ LOCAL_SHARED_LIBRARIES        := $(common_libs) \
                                   libgrallocutils \
                                   libgralloccore \
                                   libsync \
-                                  vendor.qti.hardware.display.mapper@3.0 \
+                                  vendor.qti.hardware.display.mapper@2.0 \
                                   vendor.qti.hardware.display.mapperextensions@1.0 \
                                   android.hardware.graphics.mapper@2.0 \
                                   android.hardware.graphics.mapper@2.1 \
-                                  vendor.qti.hardware.display.mapperextensions@1.1 \
-                                  android.hardware.graphics.mapper@3.0
+                                  vendor.qti.hardware.display.mapperextensions@1.1
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdgralloc\" -Wno-sign-conversion
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 LOCAL_SRC_FILES               := QtiMapper.cpp QtiMapperExtensions.cpp
@@ -114,10 +110,9 @@ LOCAL_SHARED_LIBRARIES        := $(common_libs) \
                                  libqdMetaData \
                                  libgrallocutils \
                                  libgralloccore \
-                                 vendor.qti.hardware.display.allocator@3.0 \
-                                 android.hardware.graphics.mapper@3.0 \
+                                 vendor.qti.hardware.display.allocator@1.0 \
                                  android.hardware.graphics.mapper@2.1 \
-                                 android.hardware.graphics.allocator@3.0
+                                 android.hardware.graphics.allocator@2.0
 LOCAL_CFLAGS                  := -DLOG_TAG=\"qdgralloc\" $(common_flags)
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
